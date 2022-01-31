96d0cd054497a8a3706d94f7b76512e967457243
AleksMottus <aleksmottus0@gmail.com>
2022-01-31 09:27:30 +0200

    uuendus

---
+++ php/first.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<h1>sokjfo</h1>
+<body>
+    <?php
+    $a = 6;
+    $b = 6;
+    if ($a==$b){
+    echo "kao välja";
+}else {
+        echo "tule sisse";}
+
+    ?>
+</body>
+</html>
