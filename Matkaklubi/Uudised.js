7d507d5c745d1bcfa3b78671bd1db395c7a07cee
AleksMottus <aleksmottus0@gmail.com>
2022-02-15 10:41:18 +0200

    uuendus

---
+++ Matkaklubi/Uudised.js
@@ -0,0 +1,9 @@
+const uudised = [
+    {
+        id: 0,
+        pealkiri: "Uudise pealkiri",
+        kokkuvõte: "Lühike tekst",
+        uudisetekst: "pikem tekst",
+        uudispilt: "uudise url",
+    }
+]
